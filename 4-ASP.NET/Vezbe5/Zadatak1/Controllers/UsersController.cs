95d2f761bcee746b5093a5a8346f8ead118aad2d
Dragan <milosevicdragan002@gmail.com>
2024-05-13 07:50:20 +0200

    Create UsersController.cs

---
+++ 4-ASP.NET/Vezbe5/Zadatak1/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zadatak1.Models;
+
+namespace Zadatak1.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User
+            {
+                Username = "pera",
+                Password = "pera"
+            });
+            return View(users);
+        }
+    }
+}
