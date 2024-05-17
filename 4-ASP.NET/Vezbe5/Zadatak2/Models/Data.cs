9f3e44511b8a5e55818b4446548e89edac03fc3b
Dragan <milosevicdragan002@gmail.com>
2024-05-17 15:58:23 +0200

    Read Products func

---
+++ 4-ASP.NET/Vezbe5/Zadatak2/Models/Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Zadatak2.Models
+{
+    public class Data
+    {
+        public static List<Product>ReadProducts(string path)
+        {
+            List<Product> products = new List<Product>();
+            path = HostingEnvironment.MapPath(path);
+            FileStream stream = new FileStream(path, FileMode.Open);
+            StreamReader sr = new StreamReader(stream);
+            string line = "";
+            while((line = sr.ReadLine()) != null)
+            {
+                string[] tokens = line.Split(";");
+                Product p = new Product(tokens[0], tokens[1], double.Parse(tokens[2]));
+                products.Add(p);
+            }
+            sr.Close();
+            stream.Close();
+
+            return products;
+        }
+
+        public static List<User>ReadUsers(string path)
+        {
+            List<User> users = new List<User>();
+        }
+    }
+}
