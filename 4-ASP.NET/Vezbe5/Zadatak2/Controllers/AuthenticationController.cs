db266682b3b4e43aa1ae3a5d7189b3a143de0983
Dragan <milosevicdragan002@gmail.com>
2024-05-17 16:34:17 +0200

    Register func

--- 4-ASP.NET/Vezbe5/Zadatak2/Controllers/AuthenticationController.cs
+++ 4-ASP.NET/Vezbe5/Zadatak2/Controllers/AuthenticationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zadatak2.Models;
 
 namespace Zadatak2.Controllers
 {
@@ -12,6 +13,30 @@ namespace Zadatak2.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Register()
+        {
+            User user = new User();
+            Session["user"] = user;
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            List<User> users = (List<User>)HttpContext.Application["Users"];
+            if(users.Contains(user))
+            {
+                ViewBag.Message = $"User with {user.Username} already exists!";
+                return View();
+            }
+
+            users.Add(user);
+            Data.SaveUser(user);
+            Session["user"] = user;
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
