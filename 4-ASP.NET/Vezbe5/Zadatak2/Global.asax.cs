c03058f405ffdfeec2504e26baa3075e66b7c278
Dragan <milosevicdragan002@gmail.com>
2024-05-17 16:12:40 +0200

    Update Global.asax.cs

--- 4-ASP.NET/Vezbe5/Zadatak2/Global.asax.cs
+++ 4-ASP.NET/Vezbe5/Zadatak2/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Zadatak2.Models;
 
 namespace Zadatak2
 {
@@ -13,6 +14,12 @@ namespace Zadatak2
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            List<Product> products = Data.ReadProducts("~/App_Data/products.txt");
+            HttpContext.Current.Application["products"] = products;
+
+            List<User> users = Data.ReadUsers("~/App_Data/users.txt");
+            HttpContext.Current.Application["users"] = users;
         }
     }
 }
