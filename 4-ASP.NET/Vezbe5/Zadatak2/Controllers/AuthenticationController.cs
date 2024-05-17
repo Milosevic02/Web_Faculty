98e0edf06763582fea2154678174ece0527730e4
Dragan <milosevicdragan002@gmail.com>
2024-05-17 16:42:36 +0200

    Login and Logout func

--- 4-ASP.NET/Vezbe5/Zadatak2/Controllers/AuthenticationController.cs
+++ 4-ASP.NET/Vezbe5/Zadatak2/Controllers/AuthenticationController.cs
@@ -38,5 +38,30 @@ namespace Zadatak2.Controllers
             return RedirectToAction("Index", "Home");
 
         }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            List<User> users = (List<User>)HttpContext.Application["Users"];
+            User user = users.Find(u => u.Username.Equals(username) && u.Password.Equals(password));
+
+            if (user == null)
+            {
+                ViewBag.Message = $"User with credentials does not exist!";
+                return View("Index");
+            }
+
+            Session["user"] = user;
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Logout()
+        {
+            Session["user"] = null;
+            Session["cart"] = null;
+
+            return View("Index");
+
+        }
     }
 }
